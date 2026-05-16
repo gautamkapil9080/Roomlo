52805456fbc63530eeedc909c5e871b023625599
Kapil Gautam <gautamkapil9080@gmail.com>
2026-05-16 19:50:26 +0545

    Add listing detail view and update index route for listings

--- app.js
+++ app.js
@@ -26,11 +26,47 @@ app.get("/listings",async (req,res)=>{ // index route :
         let data= await Listing.find({})
         res.render("listings/index",{data});
 })
+
+// Create and add new route :
+
+// sending request when button is clicked 
+app.get("/new",(req,res)=>{
+    res.render("listings/create");
+});
+
+// creating the end point to catch after submmision of the creation page.
+app.post("/new/add",(req,res)=>{
+    
+})
+
+
 app.get("/listings/:id",async(req,res)=>{ // To show the value 
     let {id}=req.params;
         const findValue=await Listing.findById(id);
         res.render("listings/showvalue",{findValue});
 })
+// Create and add new route :
+
+// sending request when button is clicked 
+app.get("/new",(req,res)=>{
+    res.render("listings/create");
+});
+
+// creating the end point to catch after submmision of the creation page.
+app.post("/add",async(req,res)=>{
+    let {title,description,price,country,location}=req.body;
+    const newListing=new Listing({
+        title: title,
+        description: description,
+        price: price,
+        country: country,
+        location: location
+    })
+    await newListing.save();
+    res.redirect("/listings");
+
+})
+
 
 app.listen(port,()=>{
     console.log("Server has been started");
